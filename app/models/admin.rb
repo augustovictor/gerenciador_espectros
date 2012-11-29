79034cc6c3dc40f55281029aa8cffb57dc8d2105
Victor Augusto <victoraweb@gmail.com>
2012-11-29 19:35:57 -0300

    correcao cadastro

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,9 +2,8 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-        #:registerable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :nome, :telefone, :email, :password, :password_confirmation, :remember_me
